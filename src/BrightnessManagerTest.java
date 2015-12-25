200c73faba884bbede04299421f730e7993c1fc0
user <user@HP>
2015-12-25 20:16:01 +0100

    Einfachste grafische Oberfläche hinzugefügt

--- src/BrightnessManagerTest.java
+++ src/BrightnessManagerTest.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import javax.swing.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,7 +16,12 @@ public class BrightnessManagerTest {
     
     public static void main(String[] args) throws IOException
     {
-        int x = 60;
+        //JTextField Array damit man in Zukunft um mehr Eingabefelder erweitern kann
+        JTextField[] feld = {new JTextField()};
+        Object[] msg = {"Helligkeit", feld[0]};  
+        JOptionPane.showConfirmDialog(null, msg, "BrightnessManager", JOptionPane.OK_CANCEL_OPTION);
+        int x = Integer.parseInt(feld[0].getText());
+
         BrightnessManager.setHelligkeit(x);
     }
     
